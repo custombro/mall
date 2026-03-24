e3e4264bc070a95d4485737bd472265313bfb53e
custombro <your_github_email@example.com>
2026-03-24 13:34:08 +0900

    fix(cb-mall): change mode select title to label

--- app/mode-select/page.tsx
+++ app/mode-select/page.tsx
@@ -36,7 +36,7 @@ export default function Page() {
                 제작 선택
               </p>
               <h1 className="text-4xl font-semibold leading-tight text-white sm:text-5xl">
-                바로 이동합니다.
+                모드 선택
               </h1>
               </div>
           </div>
